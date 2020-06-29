a10233dceb86eac6289342af13fe6c98dc9f14ce
yolka85 <4yolka@gmail.com>
2020-06-29 23:30:19 +0300

    Added business types and users sections

--- scripts/data.js
+++ scripts/data.js
@@ -47,5 +47,40 @@ var data = {
   },
   businessTypes: {
     heading: "Типи бізнесу",
+    items: [
+      {
+        title: "Салони краси",
+        image: "images/b-type-1.svg",
+        class: "business-types__beauty-salon"
+      },
+      {
+        title: "Кав’ярні та ресторани",
+        image: "images/b-type-2.svg",
+        class: "business-types__cafe"
+      },
+      {
+        title: "Стоматологічні клініки",
+        status: "скоро"
+      },
+      {
+        title: "Квіткові магазини",
+        status: "скоро"
+      },
+      {
+        title: "Автомийки",
+        status: "скоро"
+      },
+      {
+        title: "Парковки",
+        status: "скоро"
+      }
+    ]
+  },
+  users: {
+    title: "Демо бот",
+    text1: "Перевірте самі, як працюе чатбот і переконойтесь що це дійсно зручно.",
+    text2: "Натисніть щоб спробувати",
+    subtitle: "True Barbershop",
+    sliderText: "Бот допоможе клієнту зробити замовлення онлайн і там же сплатити його, якщо у вас кав’ярня або квітковий магазин"
   }
 };
